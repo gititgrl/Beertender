369802b808838edaa9636916c32411be06b54725
gititgirl <jordanlind2017@gmail.com>
2023-01-10 15:05:42 -0800

    basic file structure set up testing on localhost 3000

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Beertender
+Beertender is an app that allows you to search in any city for the local breweries.
---
+++ src/utils/api.js
@@ -0,0 +1,7 @@
+//Dependencies
+import axios from "axios"
+
+//API Request
+export async function getEndpoints() {
+    const { data } = await axios.get()
+}
