5b11d4aec1dc94722bb79dbcee9a0616d2add4a2
gititgirl <jordanlind2017@gmail.com>
2023-01-23 08:11:59 -0800

    styles

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-screen bg-blue-100">
         <div className="not-found">
             <h1 className="text-2xl font-righteous mb-4">Sorry!</h1>
             <p>Looks like the page you're looking for doesn't exist...</p>
